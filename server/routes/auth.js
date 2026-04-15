c6b13e46706d28d2373255c9e50fba2bf3b4000e
Rohit Khati <rohitkhati303@gmail.com>
2026-04-16 00:37:10 +0530

    updated the backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { register, login, getMe, updateProfile, verifyOTP, resendOtp } from "../controllers/authController.js";
 import auth from "../middleware/auth.js";
+import { loginLimit, optlimit, registerLimit } from "../middleware/ratelimiter.middleware.js";
+import { validateLogin, validateRegister } from "../middleware/validation.middleware.js";
 
 const router = express.Router();
+//POST /api/auth/register 
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/verify-otp", verifyOTP);
+router.post("/register", "/registerlimit","/validateregister", registerLimit, validateRegister, register)
+router.post("/login","/loginlimit","/validatelogin",loginLimit,validateLogin, login);
+router.post("/verify-otp","/otplimit",optlimit, verifyOTP);
 router.post("/resend-otp", resendOtp);
 router.get("/me", auth, getMe);
 router.put("/profile", auth, updateProfile);
