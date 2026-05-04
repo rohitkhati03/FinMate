65048afaa89b3c07bdccd4b8770d4e6255719486
Rohit Khati <rohitkhati303@gmail.com>
2026-05-04 23:18:27 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(helmet());
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URI || "http://localhost:5173",
+    origin: process.env.CLIENT_URI || "finmatelive.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
