97a4375c258fe38cb8f5c15711998d7066f995b5
Rohit Khati <rohitkhati303@gmail.com>
2026-05-04 22:58:41 +0530

    final updated for the project

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:7000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://finmate-2t8u.onrender.com'
 });
 
 API.interceptors.request.use((config) => {
